c433e09e063782c5a260785adf83ca6ee3804d91
Dante Benicio <dantecosta79@gmail.com>
2024-02-13 22:19:28 -0300

    entidades atualizadas

--- src/Leilao.API/Entities/Item.cs
+++ src/Leilao.API/Entities/Item.cs
@@ -5,7 +5,7 @@ namespace Leilao.API.Entities;
 
 [Table("Items")]
 public class Item
-{
+{ 
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Brand { get; set; } = string.Empty;
