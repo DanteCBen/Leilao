4a16fa5d4cf43ff8f195ef487f1e83c663206c96
Dante Benicio <dantecosta79@gmail.com>
2024-02-13 22:23:28 -0300

    adicionado autenticaçao no swagger e injeçoes de dependencias

--- src/Leilao.API/Program.cs
+++ src/Leilao.API/Program.cs
@@ -1,15 +1,65 @@
+using Leilao.API.Contracts;
+using Leilao.API.Filters;
+using Leilao.API.Repositories;
+using Leilao.API.Repositories.DataAccess;
+using Leilao.API.Services;
+using Leilao.API.UseCases.Leiloes.GetCurrent;
+using Leilao.API.UseCases.Offers.CreateOffer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the bearer scheme.
+                        Enter 'Bearer' [space] and then your token in the text input below.
+                        Example: 'Bearer 12345bcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
+
+builder.Services.AddScoped<AuthenticationUserAttribute>();
+builder.Services.AddScoped<ILoggedUser ,LoggedUser>();
+builder.Services.AddScoped<CreateOfferUseCase>();
+builder.Services.AddScoped<GetCurrentLeilaoUseCase>();
+builder.Services.AddScoped<IAuctionRepository, LeilaoRepository>();
+builder.Services.AddScoped<IOfferRepository, OfferRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddDbContext<LeilaoDbContext>(options =>
+{
+    options.UseSqlite("Data Source=C:\\Users\\Dante\\Downloads\\leilaoDbNLW.db");
+});
+
+builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
